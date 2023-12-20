805fd71990188ee3a52f278187c0362b02ca7f5f
Brian <brianmariga0@gmail.com>
2023-12-20 12:29:49 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(text){
+    return text.toUpperCase();
+}
+
+function whisper(textOne){
+    return textOne.toLowerCase();
+}
+
+function logShout(textTwo){
+    console.log(textTwo.toUpperCase())
+}
+
+function logWhisper(textThree){
+    console.log(textThree.toLowerCase())
+}
+
+function sayHiToHeadphonedRoommate(textFour){
+    return "I can't hear you!"
+    
+}
